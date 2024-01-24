8c5c9b8b73fa6c27626086016ce94da4c871338d
박준형 <abide0206@naver.com>
2024-01-25 00:52:56 +0900

    [level 2] Title: 소수 찾기, Time: 0.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,22 @@
 from itertools import permutations
 
 def checkPrime(n):
-    if n < 2:
+    if n<2:
         return False
-    for i in range(2, int(n**(0.5)+1)):
+    for i in range(2, int(n**(1/2)+1)):
         if n % i == 0:
             return False
     return True
 
 def solution(numbers):
-    answer = []
-    temp = []
-    
-    for i in range(1, len(numbers)+1):
-        temp += list(permutations(numbers, i))
-        
-    num = [int(''.join(t)) for t in temp]
-    
-    for i in num:
-        if checkPrime(i):
-            answer.append(i)
-    return len(set(answer))
+    answer = set()
+    temp = [int(i) for i in numbers]
+
+    for j in range(1, len(temp) + 1):
+        perms = list(permutations(temp, j))
+        for perm in perms:
+            num = int(''.join(map(str, perm)))
+            if checkPrime(num):
+                answer.add(num)
+
+    return len(answer)
