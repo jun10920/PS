a8a31744342537ecf8756356306ac2308b309616
박준형 <abide0206@naver.com>
2024-01-25 16:44:18 +0900

    [level 1] Title: 모의고사, Time: 2.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2.63 ms
+메모리: 10.3 MB, 시간: 2.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 13:46:56
+2024년 1월 4일 16:44:16
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,18 @@
 def solution(answers):
-    a1 = [1,2,3,4,5]
-    a2 = [2,1,2,3,2,4,2,5]
-    a3 = [3,3,1,1,2,2,4,4,5,5]
-    c1, c2, c3 = 0,0,0
-    for i in range(len(answers)):
-        s1 = i % 5
-        s2 = i % 8
-        s3 = i % 10
-        
-        if a1[s1] == answers[i]:
-            c1 +=1
-        if a2[s2] == answers[i]:
-            c2 +=1
-        if a3[s3] == answers[i]:
-            c3 +=1
-    k = max(c1,c2,c3)
-    answer = []
-    if k == c1:
-        answer.append(1)
-    if k == c2:
-        answer.append(2)
-    if k == c3:
-        answer.append(3)
-    answer.sort()
-    return answer
+    arr1 = [1,2,3,4,5]
+    arr2 = [2,1,2,3,2,4,2,5]
+    arr3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    scores = [0,0,0]
+    
+    for i, v in enumerate(answers):
+        if v == arr1[i%5]:
+            scores[0] += 1
+        if v == arr2[i%8]:
+            scores[1] += 1
+        if v == arr3[i%10]:
+            scores[2] += 1
+    
+    highest = max(scores)
+
+    return [i+1 for i,v in enumerate(scores) if v == highest]
