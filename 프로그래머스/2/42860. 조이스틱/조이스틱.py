3942b58658cc60539cb04ced93a9024f755e9d37
박준형 <abide0206@naver.com>
2024-02-01 23:19:53 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,17 +1,21 @@
 def solution(name):
     answer = 0
+    # 상하 조작횟수 구하기
     for i in name:
         answer += min(ord(i)-ord('A'), ord('Z')-ord(i)+1)
-
-    tmp=0
-    shift=len(name)-1
+        
+    print(answer)
+    # 좌우 조작횟수 구하기
+    sum = len(name) -1
     for i in range(len(name)):
-        if name[i]=="A":
-            tmp=i
-            while(tmp<len(name) and name[tmp]=="A"):
-                tmp+=1
+        temp = 0
+        if name[i] == 'A':
+            temp = i
+            while temp < len(name) and name[temp] == 'A':
+                temp += 1
             left=(0 if i==0 else i-1)
-            right=len(name)-tmp
-            shift=min(shift,left+right+min(left,right))
-    answer+=shift
+            right = len(name) - temp
+            sum = min(sum, left + right + min(left, right))
+    answer += sum
+    print(answer)
     return answer
