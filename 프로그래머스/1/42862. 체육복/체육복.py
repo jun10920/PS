7fcee2f4ef4cd1abf45b1b634207dafdd0c5ecfe
박준형 <abide0206@naver.com>
2024-02-01 22:52:49 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,14 +1,18 @@
-
 def solution(n, lost, reserve):
-    _reserve = [r for r in reserve if r not in lost]
-    _lost = [l for l in lost if l not in reserve]
-    _reserve.sort()
-    _lost.sort()
-    for r in _reserve:
-        f = r - 1
-        b = r + 1
-        if f in _lost:
-            _lost.remove(f)
-        elif b in _lost:
-            _lost.remove(b)
-    return n - len(_lost)
+    
+    totalPeopleNum = n
+    
+    lostArr = [i for i in lost if i not in reserve]
+    reserveArr = [i for i in reserve if i not in lost]
+    
+    lostArr.sort()
+    reserveArr.sort()
+    
+    for i in reserveArr:
+        if i-1 in lostArr:
+            lostArr.remove(i-1)
+        elif i+1 in lostArr:
+            lostArr.remove(i+1)
+    print(lostArr)
+
+    return n-len(lostArr)
