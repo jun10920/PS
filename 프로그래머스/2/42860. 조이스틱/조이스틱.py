06bdbecf0e8f5bc7e1d0b54ee1f02021743ccdc5
박준형 <abide0206@naver.com>
2024-02-09 16:16:42 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,21 +1,20 @@
 def solution(name):
-    answer = 0
-    # 상하 조작횟수 구하기
-    for i in name:
-        answer += min(ord(i)-ord('A'), ord('Z')-ord(i)+1)
-        
-    print(answer)
-    # 좌우 조작횟수 구하기
-    sum = len(name) -1
-    for i in range(len(name)):
-        temp = 0
-        if name[i] == 'A':
-            temp = i
-            while temp < len(name) and name[temp] == 'A':
-                temp += 1
-            left=(0 if i==0 else i-1)
-            right = len(name) - temp
-            sum = min(sum, left + right + min(left, right))
-    answer += sum
-    print(answer)
-    return answer
+    result = 0
+    min_move = len(name) - 1  # 최소 이동 횟수 초기값 설정: 오른쪽으로만 이동하는 경우
+
+    for i, char in enumerate(name):
+        # 각 문자를 원하는 알파벳으로 변경하기 위한 조작 횟수 계산
+        result += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+
+        # 다음 문자부터 연속된 A의 마지막 인덱스 찾기
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+
+        # 현재 위치에서 오른쪽 또는 왼쪽으로 이동하는 경우 중 최소 이동 횟수 계산
+        # min_move = min(현재까지의 이동 횟수 + (전체 길이 - 연속된 A 이후의 인덱스), 기존의 최소 이동 횟수)
+        # i*2는 현재 위치로 돌아오기 위한 이동 횟수를 뜻함
+        min_move = min(min_move, i + len(name) - next + min(i, len(name) - next))
+
+    result += min_move  # 알파벳 변경을 위한 조작 횟수와 커서 이동을 위한 조작 횟수를 더함
+    return result
