c779e475f27e1639c03cea5c6b2bbc0fce50b0b2
박준형 <abide0206@naver.com>
2024-02-09 16:39:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,3 @@
 def solution(nums):
     howMany = len(nums)//2
-    
-    arr = list(set(nums))
-    result = len(arr) if len(arr) <= howMany else howMany
-    print(result)
-    return result
+    return min(howMany, len(set(nums)))
