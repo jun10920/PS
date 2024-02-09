2015ef4404b52f9610555982e38a8de6c4f149ad
박준형 <abide0206@naver.com>
2024-02-09 13:57:29 +0900

    [level 1] Title: 모의고사, Time: 4.92 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,21 @@
-def solution(answer):
-    arr1 = [1,2,3,4,5]
-    arr2 = [2,1,2,3,2,4,2,5]
-    arr3 = [3,3,1,1,2,2,4,4,5,5]
-    
-    score = [0,0,0]
-    
-    for i in range(len(answer)):
-        if answer[i] == arr1[i%5]:
-            score[0] +=1
-        if answer[i] == arr2[i%8]:
-            score[1] +=1
-        if answer[i] == arr3[i%10]:
-            score[2] +=1
+from itertools import cycle
+
+arr = [
+    cycle([1,2,3,4,5]),
+    cycle([2,1,2,3,2,4,2,5]),
+    cycle([3,3,1,1,2,2,4,4,5,5])
+]
+score = [0,0,0]
+
+def solution(answers):
+    for i in answers:
+        for j in range(3):
+            if i == next(arr[j]):
+                score[j] += 1
+
     highest = max(score)
-    
-    result = [i +1  for i, v in enumerate(score) if v == highest]   
-   
+    result = []
+    for i in range(len(score)):
+        if score[i] == highest:
+            result.append(i+1)
     return result
