7599de3e3d15a76844fc17b9b21925d9b9f696fe
박준형 <abide0206@naver.com>
2024-02-09 17:08:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.49 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+def solution(participant, completion):
+    answer = ''
+    dict_result=Counter(participant)-Counter(completion)
+    return list(dict_result.keys())[0]   
