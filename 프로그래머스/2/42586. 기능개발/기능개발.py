05e13b4f735e6e4077c19909718d1122c71d1029
박준형 <abide0206@naver.com>
2024-02-12 17:13:41 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,15 @@
-from collections import deque
-
-def solution(prog, speeds):
+def solution(progresses, speeds):
+    days = []
+    for progress, speed in zip(progresses, speeds):
+        day = -((progress - 100)// speed)
+        days.append(day)
+    days.reverse()
     answer = []
-    queue1 = deque()
-    queue2 = deque()
-    for i in prog:
-        queue1.append(i)
-    for i in speeds:
-        queue2.append(i)
-    while queue1:
-        sum = 0
-        while queue1 and queue1[0] >= 100:
-            queue1.popleft()
-            queue2.popleft()
-            sum += 1
-        if sum != 0:
-            answer.append(sum)
-        sum = 0
-       
-        for i in range(len(queue1)):
-            if queue1[i] < 100:
-                queue1[i] += queue2[i]
+    while days:
+        current_day = days.pop()
+        count = 1
+        while days and days[-1] <= current_day:
+            days.pop()
+            count += 1
+        answer.append(count)     
     return answer
