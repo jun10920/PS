1b71645b5426b870fa65839ecb60381425fa46ea
박준형 <abide0206@naver.com>
2024-02-13 01:31:26 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,15 @@
+def solution(citations):
+    # 인용 횟수를 내림차순으로 정렬
+    citations.sort(reverse=True)
+    
+    # H-Index 찾기
+    h_index = 0
+    for i, citation in enumerate(citations):
+        # 인용 횟수가 논문 수(인덱스+1)보다 크거나 같은 경우, H-Index 후보로 간주
+        if citation >= i + 1:
+            h_index = i + 1
+        else:
+            # 나머지 논문은 h번 이하 인용되었으므로 반복 중지
+            break
+    
+    return h_index
