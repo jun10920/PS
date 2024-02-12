ffe26cc9c91a093fd60013a8c8de8a38d3f4ab51
박준형 <abide0206@naver.com>
2024-02-12 17:03:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 140.05 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,12 +1,7 @@
 def solution(arr):
-    # 결과를 저장할 리스트 초기화
-    result = [arr[0]]
-
-    # 배열 arr의 두 번째 원소부터 확인
-    for num in arr[1:]:
-        # 현재 숫자가 결과 리스트의 마지막 원소와 다르면 추가
-        if num != result[-1]:
-            result.append(num)
-
-    # 수정된 결과 반환
-    return result
+    stack = [arr[0]]
+    for i in arr[1:]:
+        while stack and stack[-1] == i:
+            stack.pop()
+        stack.append(i)
+    return stack
