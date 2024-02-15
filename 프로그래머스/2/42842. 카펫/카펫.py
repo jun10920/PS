118f14b999f485af81715c5437f7779edcfa7e42
박준형 <abide0206@naver.com>
2024-02-15 20:38:55 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,7 @@
 def solution(brown, yellow):
-    # y는 카펫의 세로 길이, x는 카펫의 가로 길이
-    for y in range(3, int((brown + yellow) ** 0.5) + 1):
-        if (brown + yellow) % y == 0:  # 전체 카펫의 면적이 y로 나누어 떨어지는 경우
+    for i in range(3, int((brown + yellow)**(1/2)) + 1):
+        if (brown + yellow) % i == 0:
+            y = i
             x = (brown + yellow) // y
-            if (x - 2) * (y - 2) == yellow:  # 노란색 격자 수 조건을 만족하는지 확인
+            if (x - 2) * (y - 2) == yellow:
                 return [x, y]
