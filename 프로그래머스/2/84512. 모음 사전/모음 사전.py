f7c14d8a60ce102f7da5f6fa05c99e120bad550e
박준형 <abide0206@naver.com>
2024-02-15 23:52:07 +0900

    [level 2] Title: 모음 사전, Time: 1.28 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,12 @@
 from itertools import product
 
 def solution(word):
-    characters = ['A', 'E', 'I', 'O', 'U']
-    arr = []
-
-    # 1글자에서 5글자까지 가능한 모든 조합을 생성하여 arr에 추가
-    for i in range(1, 6):
-        for combination in product(characters, repeat=i):
-            arr.append(''.join(combination))
-
-    arr.sort()  # 생성된 문자열을 사전 순으로 정렬
-    answer = arr.index(word) + 1  # 주어진 word의 인덱스를 찾고, 문제의 조건에 맞게 1을 더함
+    arr = ['A', 'E', 'I', 'O', 'U']
+    answerArr = []
+    for i in range(1, len(arr)+ 1):
+        answerArr += list(product(arr, repeat = i))
+    
+    answerArr = list(map(lambda x : ''.join(x), answerArr))
+    answerArr.sort()
+    answer = answerArr.index(word) + 1
     return answer
