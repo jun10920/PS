27e66f724cd301b38ab0e05a14e1b9a0324b26e3
박준형 <abide0206@naver.com>
2024-02-16 00:49:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -4,33 +4,31 @@ def solution(operations):
     min_heap = []
     max_heap = []
     sync = {}
-    
-    for op in operations:
-        command, value = op.split()
+    for i in operations:
+        command, value = i.split()
         value = int(value)
         
         if command == 'I':
             heapq.heappush(min_heap, value)
             heapq.heappush(max_heap, -value)
             sync[value] = sync.get(value, 0) + 1
-        elif value == -1 and min_heap:
-            while min_heap and sync[min_heap[0]] == 0:
-                heapq.heappop(min_heap)
-            if min_heap:
-                sync[min_heap[0]] -= 1
-                heapq.heappop(min_heap)
-        elif value == 1 and max_heap:
-            while max_heap and sync[-max_heap[0]] == 0:
-                heapq.heappop(max_heap)
-            if max_heap:
-                sync[-max_heap[0]] -= 1
-                heapq.heappop(max_heap)
-                
+        else:
+            if value == -1 and min_heap:
+                while min_heap and sync[min_heap[0]] == 0:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    sync[heapq.heappop(min_heap)] -= 1
+            if value == 1 and max_heap:
+                while max_heap and sync[-max_heap[0]] == 0:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    sync[-heapq.heappop(max_heap)] -= 1
     while min_heap and sync[min_heap[0]] == 0:
         heapq.heappop(min_heap)
     while max_heap and sync[-max_heap[0]] == 0:
         heapq.heappop(max_heap)
-    if not min_heap:
-        return [0, 0]
+        
+    if not min_heap or not max_heap:
+        return [0,0]
     else:
         return [-max_heap[0], min_heap[0]]
