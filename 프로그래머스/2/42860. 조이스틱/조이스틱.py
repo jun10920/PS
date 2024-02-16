aa3c8716b481987909e44837f2e0a2cdd6a9a927
박준형 <abide0206@naver.com>
2024-02-16 23:29:44 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,17 +1,21 @@
 def solution(name):
+    
     result = 0
+    
     for i in name:
         result += min(ord(i) - ord('A'), ord('Z') - ord(i) + 1)
     
-    default = len(name) - 1
-    
+    LR = len(name) - 1
+    startIdx = 0
+    temp = 0
     for i in range(len(name)):
         if name[i] == 'A':
+            startIdx = i
             temp = i
+            print(temp)
             while temp < len(name) and name[temp] == 'A':
                 temp += 1
-            left = 0 if i == 0 else i - 1
+            left = 0 if i==0 else startIdx-1
             right = len(name) - temp
-            default = min(default, left + right + min(left, right))
-    
-    return result + default
+            LR = min(LR, left+right+min(left, right))
+    return result + LR
