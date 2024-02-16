3faae2395f61e8a87e8a9f61adde368518c70b98
박준형 <abide0206@naver.com>
2024-02-16 23:37:26 +0900

    [level 2] Title: 전화번호 목록, Time: 84.47 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,6 @@
-def solution(pb):
-    answer = True
-    pb.sort()
-    for i in range(len(pb)- 1):
-        if pb[i+1].startswith(pb[i]):
-            answer = False
-    return answer
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    return True
