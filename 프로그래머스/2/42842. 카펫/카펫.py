b6eade526cd2f5a15dc3336bb1906ef3db3533dc
박준형 <abide0206@naver.com>
2024-02-18 00:38:27 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,9 @@
 def solution(brown, yellow):
-    for i in range(3, int((brown + yellow)**(1/2)) + 1):
+    for i in range(3, int(brown + yellow**(0.5)) + 1):
         if (brown + yellow) % i == 0:
-            y = i
+            y = (brown + yellow) // i
             x = (brown + yellow) // y
-            if (x - 2) * (y - 2) == yellow:
-                return [x, y]
+            if (x-2)*(y-2) == yellow and x >= y:
+                return [x,y]
+    
+            
