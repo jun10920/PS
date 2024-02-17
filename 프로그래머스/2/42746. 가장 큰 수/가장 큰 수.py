f58eb4eea2cfed5dac01d6e01538fe515ae437b5
박준형 <abide0206@naver.com>
2024-02-17 10:00:02 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,7 @@
 def solution(numbers):
-    strN = list(map(str,numbers))
-    strN.sort(key=lambda x: x*3, reverse = True)
-    answer = ''.join(strN)
+    
+    numbers = list(map(str,numbers))
+    numbers.sort(key = lambda x: x*3, reverse=True)
+    answer = ''.join(numbers)
+   
     return answer if answer[0] != '0' else '0'
