5bea207012d88b39561c5e122e83cae22badf0b0
박준형 <abide0206@naver.com>
2024-02-18 01:04:18 +0900

    [level 2] Title: 모음 사전, Time: 1.40 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,9 +1,8 @@
 from itertools import product
-
 def solution(word):
     arr = ['A', 'E', 'I', 'O', 'U']
     answerArr = []
-    for i in range(1, len(arr)+ 1):
+    for i in range(1, len(arr) + 1):
         answerArr += list(product(arr, repeat = i))
     
     answerArr = list(map(lambda x : ''.join(x), answerArr))
