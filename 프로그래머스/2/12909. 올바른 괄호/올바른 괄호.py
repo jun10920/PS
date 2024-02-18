814f4b7b098debcf083c8eaf941caa955d361c24
박준형 <abide0206@naver.com>
2024-02-18 19:44:34 +0900

    [level 2] Title: 올바른 괄호, Time: 6.80 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,8 +1,10 @@
 def solution(s):
     answer = True
+    stack = []
+    
     if s[0] == ')' or s[-1] == '(':
         return False
-    stack = []
+    
     for i in s:
         if i == '(':
             stack.append(i)
@@ -10,4 +12,8 @@ def solution(s):
             if not stack:
                 return False
             stack.pop()
-    return not stack
+    
+    if not stack:
+        return True
+    
+    return False
