0e7f5e403cadc19c46a2e05666be9664d6ed0281
박준형 <abide0206@naver.com>
2024-02-20 20:22:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,34 +1,22 @@
 import heapq
 
 def solution(operations):
-    min_heap = []
-    max_heap = []
-    sync = {}
+    heap = []
+    
     for i in operations:
         command, value = i.split()
         value = int(value)
         
         if command == 'I':
-            heapq.heappush(min_heap, value)
-            heapq.heappush(max_heap, -value)
-            sync[value] = sync.get(value, 0) + 1
-        else:
-            if value == -1 and min_heap:
-                while min_heap and sync[min_heap[0]] == 0:
-                    heapq.heappop(min_heap)
-                if min_heap:
-                    sync[heapq.heappop(min_heap)] -= 1
-            if value == 1 and max_heap:
-                while max_heap and sync[-max_heap[0]] == 0:
-                    heapq.heappop(max_heap)
-                if max_heap:
-                    sync[-heapq.heappop(max_heap)] -= 1
-    while min_heap and sync[min_heap[0]] == 0:
-        heapq.heappop(min_heap)
-    while max_heap and sync[-max_heap[0]] == 0:
-        heapq.heappop(max_heap)
+            heapq.heappush(heap, value)
+        elif heap:
+            if value == -1:
+                heapq.heappop(heap)
+            else:
+                heap.remove(max(heap))
+        heapq.heapify(heap)
         
-    if not min_heap or not max_heap:
-        return [0,0]
+    if not heap:
+        return [0, 0]
     else:
-        return [-max_heap[0], min_heap[0]]
+        return [max(heap), heap[0]] 
