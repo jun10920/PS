ffefeefcd2c6fd17ec3b61e28bf893e035baa911
박준형 <abide0206@naver.com>
2024-02-22 16:22:59 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,10 +1,13 @@
-WITH RECURSIVE HourSeries AS (
-    SELECT 0 AS Hour
-    UNION ALL
-    SELECT Hour + 1 FROM HourSeries WHERE Hour < 23
+with recursive time as (
+    select 0 as hour
+    union
+    select hour+1 from time where hour<23
 )
-SELECT hs.Hour, COUNT(ao.Animal_ID) AS AdoptionCount
-FROM HourSeries hs
-LEFT JOIN (SELECT *, HOUR(DATETIME) AS Hour FROM ANIMAL_OUTS) ao ON hs.Hour = ao.Hour
-GROUP BY hs.Hour
-ORDER BY hs.Hour;
+
+select time.hour, count(animal_id) as count
+from (
+    select *, hour(datetime) as hour
+    from animal_outs) a
+    right join time on a.hour=time.hour
+group by time.hour
+order by time.hour
