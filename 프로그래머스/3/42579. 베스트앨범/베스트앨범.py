512e86e3e8a1998fce4f8eb82f7be547673fd1aa
박준형 <abide0206@naver.com>
2024-02-23 01:07:17 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,19 +1,17 @@
 from collections import defaultdict
 def solution(genres, plays):
-    answer = []
     
     arr1 = defaultdict(int)
     arr2 = defaultdict(list)
     
     for i, (g, p) in enumerate(zip(genres, plays)):
-        arr1[g] += p
-        arr2[g].append((i,p)) 
+        arr1[g] = arr1.get(g, 0) + p
+        arr2[g].append((p, i))
         
-    arr1 = sorted(arr1, key= arr1.get, reverse=True)
-    print(arr2)
-    for g in arr1:
-        sorted_songs= sorted(arr2[g], key = lambda x: (-x[1], x[0]))
-        answer += [s[0] for s in sorted_songs[:2]]
-    print(answer)
+    sgenre = sorted(arr1, key = arr1.get, reverse = True)
     
+    answer = []
+    for i in sgenre:
+        answer += sorted(arr2[i], key = lambda x: (-x[0], x[1]))[:2]
+    answer = list(map(lambda x: x[1], answer))
     return answer
