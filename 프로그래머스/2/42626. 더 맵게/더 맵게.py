b0395990deed3e201ea3671e4c2ca22d2717f1cf
박준형 <abide0206@naver.com>
2024-02-23 19:03:59 +0900

    [level 2] Title: 더 맵게, Time: 1660.30 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,17 @@
 import heapq
 
 def solution(s, k):
-    h = heapq
-    h.heapify(s)
     
-    
-    
+    heapq.heapify(s)
     count = 0
+    
     while s[0] < k:
         if len(s) < 2:
-            return -1   
-        first = h.heappop(s)
-        second = h.heappop(s)
-        new = first + (second * 2)
-        h.heappush(s, new)
+            return -1
+        first = heapq.heappop(s)
+        second = heapq.heappop(s)
+        new= first + (second * 2)
+        heapq.heappush(s, new)
         count += 1
-
+        
     return count
