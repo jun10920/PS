6bc74533029a6b23655fa7394ad08000d58dce8f
박준형 <abide0206@naver.com>
2024-03-13 16:34:01 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -10,16 +10,15 @@ def bfs(begin, target, words):
             return count
         
         for word in words[:]:
-            if word_diff_by_one(current_word, word):
+            if diff_by_one(current_word, word):
                 queue.append((word, count + 1))
-                words.remove(word)
-    return 0
 
-def word_diff_by_one(word1, word2):
-    diff_count = sum(1 for a, b in zip(word1, word2) if a != b)
-    return diff_count == 1
+def diff_by_one(word1, word2):
+    cnt = sum(1 for a, b in zip(word1, word2) if a != b)
+    return cnt == 1
 
 def solution(begin, target, words):
     if target not in words:
         return 0
-    return bfs(begin, target, words)
+    else:
+        return bfs(begin, target, words)
