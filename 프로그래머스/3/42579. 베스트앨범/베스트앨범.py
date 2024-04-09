de0f447c70578a478d62bbd9dc7f7c392b21e701
PARKJUNHYOUNG <abide0206@naver.com>
2024-04-09 19:53:00 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,17 +1,40 @@
-from collections import defaultdict
 def solution(genres, plays):
-    
-    arr1 = defaultdict(int)
-    arr2 = defaultdict(list)
-    
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        arr1[g] = arr1.get(g, 0) + p
-        arr2[g].append((p, i))
+    category_array ={} # 같은 카테고리는 배열로 체이닝
+    category_sum ={} # 카테고리별 총합
+		
+		
+    for index in range(len(genres)) : # O(n)
+        category = genres[index] # key
         
-    sgenre = sorted(arr1, key = arr1.get, reverse = True)
-    
+        # 해시 맵 안에 카테고리로 된 키가 있을 때
+        if category in category_array and category in category_sum :
+            
+            array = category_array[category] # 체이닝 배열 꺼내기.
+            array.append(index) #인덱스 삽입 후 정렬
+            
+            for i in range(len(array)-1) : #선택정렬 
+                max = i
+                for j in range(i+1,len(array)) :
+                    if plays[array[max]] < plays[array[j]] : # 같은 경우는 스왑 x 더 작은 인덱스가 살아 남음.
+                        max = j 
+                array[i], array[max] = array[max], array[i] # swap
+
+            if len(array) > 2 : array.pop() #정렬 후 배열 길이 2로 유지.
+
+            category_array[category]= array # 다시 넣기
+            category_sum[category] += plays[index] # 더하기
+
+        else : # 해시 맵 안에 카테고리로 된 키가 없을 때 -> 초기화
+            category_array[category] = [index]
+            category_sum[category] = plays[index]
+		
+		#카테고리별로 재생수 총합 퀵정렬 -> O(nlogn)
+    sorted_category = sorted(category_sum.items(), key=lambda item: item[1], reverse=True)
+
     answer = []
-    for i in sgenre:
-        answer += sorted(arr2[i], key = lambda x: (-x[0], x[1]))[:2]
-    answer = list(map(lambda x: x[1], answer))
+
+    #카테고리 안에 있는 체이닝 배열 붙혀서 반환.
+    for key,value in sorted_category :
+        answer += category_array[key]
+
     return answer
