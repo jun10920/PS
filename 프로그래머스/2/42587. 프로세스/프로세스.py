acc30b380b8e480cf35111cf77a4155b673ee2d1
PARKJUNHYOUNG <abide0206@naver.com>
2024-04-13 22:26:49 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,18 +1,19 @@
 from collections import deque
 
-def solution(priorities, location):
-    # (우선순위, 초기 위치) 형태로 큐에 프로세스 정보를 저장
-    queue = deque([(priority, i) for i, priority in enumerate(priorities)])
-    answer = 0  # 실행 순서를 카운트할 변수
-
-    while queue:
-        current = queue.popleft()  # 현재 프로세스를 큐에서 꺼냄
-        # any 함수를 사용하여 현재 프로세스보다 우선순위가 높은 프로세스가 큐에 있는지 검사
-        if any(current[0] < other[0] for other in queue):
-            queue.append(current)  # 우선순위가 더 높은 프로세스가 있다면 큐의 끝에 다시 추가
+def solution(p, l):
+    
+    list = deque()
+    count = 0
+    
+    for i, v in enumerate(p):
+        list.append((v,i))   
+    
+    while list:
+        i = list.popleft()
+        if any(i[0] < other[0] for other in list):
+            list.append(i)
         else:
-            answer += 1  # 현재 프로세스를 실행하므로 카운터 증가
-            if current[1] == location:  # 현재 프로세스가 찾고 있는 프로세스라면
-                return answer  # 현재까지의 실행 순서를 반환
-
-    return answer
+            count += 1
+            
+            if i[1] == l:
+                return count
