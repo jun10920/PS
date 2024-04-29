74bda82c45e8587f8dc37751248fb310e39987b3
PARKJUNHYOUNG <abide0206@naver.com>
2024-04-30 01:30:51 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,10 @@
 def solution(c):
     c.sort(reverse = True)
     answer = 0
-    for i, v in enumerate(c):
+    for i,v in enumerate(c):
         if v >= i + 1:
             answer = i + 1
-            
-    if c[-1] > len(c):
-        answer =len(c)
     
+    if c[-1] > len(c):
+        answer = len(c)
     return answer
