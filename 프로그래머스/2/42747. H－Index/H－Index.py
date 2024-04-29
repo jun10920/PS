4b1550cb049fcc6695f3ee42853500c0b5d9ca9a
PARKJUNHYOUNG <abide0206@naver.com>
2024-04-30 01:39:56 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,23 @@
-def solution(c):
-    c.sort(reverse = True)
-    answer = 0
-    for i,v in enumerate(c):
-        if v >= i + 1:
-            answer = i + 1
+def solution(citations):
+    citations.sort(reverse=True)
     
-    if c[-1] > len(c):
-        answer = len(c)
-    return answer
+    for i, v in enumerate(citations):
+        if i >= v:
+            return i 
+    return len(citations)
+
+def solution(citations):
+    # 인용 횟수를 내림차순으로 정렬
+    citations.sort(reverse=True)
+    
+    # H-Index 찾기
+    h_index = 0
+    for i, citation in enumerate(citations):
+        # 인용 횟수가 논문 수(인덱스+1)보다 크거나 같은 경우, H-Index 후보로 간주
+        if citation >= i + 1:
+            h_index = i + 1
+        else:
+            # 나머지 논문은 h번 이하 인용되었으므로 반복 중지
+            break
+    
+    return h_index
