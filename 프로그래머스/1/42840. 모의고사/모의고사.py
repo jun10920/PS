6656f86847e6c40184ae397250e4994b41325032
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-01 01:42:17 +0900

    [level 1] Title: 모의고사, Time: 2.49 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,21 +1,18 @@
-from itertools import cycle
-
-arr = [
-    cycle([1,2,3,4,5]),
-    cycle([2,1,2,3,2,4,2,5]),
-    cycle([3,3,1,1,2,2,4,4,5,5])
-]
-score = [0,0,0]
-
 def solution(answers):
-    for i in answers:
-        for j in range(3):
-            if i == next(arr[j]):
-                score[j] += 1
-
-    highest = max(score)
-    result = []
-    for i in range(len(score)):
-        if score[i] == highest:
-            result.append(i+1)
-    return result
+    man1 = [1, 2, 3, 4, 5]
+    man2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    score = [0, 0, 0]
+    
+    for i in range(len(answers)):
+        if answers[i] == man1[i%5]:
+            score[0] += 1
+        if answers[i] == man2[i%8]:
+            score[1] += 1
+        if answers[i] == man3[i%10]:
+            score[2] += 1
+    
+    maxAn = max(score)
+    
+    return [i + 1 for i, v in enumerate(score) if v == maxAn]
