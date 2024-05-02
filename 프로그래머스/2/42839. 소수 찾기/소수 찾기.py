01ea7ebac6329c699a8946351d8d0349ead1ae2d
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-03 02:40:50 +0900

    [level 2] Title: 소수 찾기, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,26 +1,24 @@
 from itertools import permutations
 
-def check(num):
-    if num < 2:
+def check(n):
+    if n < 2:
         return False
     else:
-        for i in range(2, int(num**(1/2) + 1)):
-            if num % i == 0:
+        for i in range(2, int(n**(1/2)) + 1):
+            if n % i == 0:
                 return False
         return True
 
 def solution(numbers):
-    count = 0
     arr = []
+    
     for i in range(1, len(numbers) + 1):
-        arr += [i for i in permutations(numbers, i)]
-    
-    num = [int(''.join(t)) for t in arr]
-    
+        arr += list(set([i for i in permutations(numbers, i)]))
+        
+    num = [int(''.join(i)) for i in arr]
     result = set()
     for i in num:
         if check(i):
             result.add(i)
-            
-    
+
     return len(result)
