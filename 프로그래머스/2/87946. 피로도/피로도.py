3711653153d121770dcb4caf048cd4f2ef6331d2
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-03 03:16:20 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,17 @@
 from itertools import permutations
-def solution(k, dun):
-    arr = list(permutations(dun, len(dun)))
-    count= 0
+
+def solution(k, dungeons):
+    count = 0
+    arr = list(permutations(dungeons, len(dungeons)))
+    
     for i in arr:
         hp = k
         countImpl = 0
+        
         for j in i:
             if hp >= j[0]:
                 hp -= j[1]
                 countImpl += 1
-            count = max(count, countImpl)
+        count = max(count, countImpl)
+    
     return count
