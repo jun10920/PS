9d182e5a742385d08d67960d34516c401679e15b
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-03 23:32:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,44 +1,36 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
-def bfs(graph, start):
-    visited = set([start])
-    queue = deque([start])
-    count = 1  # 시작 노드를 포함하여 카운트 시작
-
-    while queue:
-        node = queue.popleft()
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append(neighbor)
-                count += 1
+def dfs(network, wire, visited):
+    visited.add(wire)
+    
+    count = 1
+    for i in network[wire]:
+        if i not in visited:
+            count += dfs(network, i, visited)
     return count
+    
 
 def solution(n, wires):
-    # 그래프 초기화
-    graph = defaultdict(list)
-    for v1, v2 in wires:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
-
-    # 최소 차이를 최대로 설정
+    network = defaultdict(list)
+    
+    for i in range(len(wires)):
+        network[wires[i][0]].append(wires[i][1]) 
+        network[wires[i][1]].append(wires[i][0]) 
+    
     min_diff = n
-
-    # 전선 순회 및 제거
-    for v1, v2 in wires:
-        # 전선 제거
-        graph[v1].remove(v2)
-        graph[v2].remove(v1)
-
-        # 송전탑 개수 계산
-        count = bfs(graph, v1)
-
-        # 송전탑 개수 차이 계산 및 최소 차이 업데이트
+    
+    for i in range(len(wires)):
+        network[wires[i][0]].remove(wires[i][1]) 
+        network[wires[i][1]].remove(wires[i][0])
+        
+        visited = set()
+        
+        count = dfs(network, wires[i][0], visited)
         diff = abs(count - (n - count))
-        min_diff = min(min_diff, diff)
-
-        # 전선 복원
-        graph[v1].append(v2)
-        graph[v2].append(v1)
-
+        
+        min_diff = min(diff, min_diff)
+        
+        network[wires[i][0]].append(wires[i][1]) 
+        network[wires[i][1]].append(wires[i][0]) 
+        
     return min_diff
