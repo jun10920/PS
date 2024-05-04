b122237cf5a0a82e09ed13ea478a9d1cbe228bf2
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-04 22:48:01 +0900

    [level 2] Title: 구명보트, Time: 8.41 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,12 +1,11 @@
 def solution(people, limit):
-    people.sort()  # 사람들의 몸무게를 오름차순으로 정렬
-    left, right = 0, len(people) - 1  # 투 포인터 초기화
-    count = 0
-
+    
+    people.sort()
+    left, right, count = 0, len(people) - 1, 0
+    
     while left <= right:
         if people[left] + people[right] <= limit:
-            left += 1  # 가장 가벼운 사람을 다음 사람으로 업데이트
-        right -= 1  # 가장 무거운 사람을 이전 사람으로 업데이트
-        count += 1  # 보트 수 증가
-
+            left += 1
+        right -= 1
+        count += 1
     return count
