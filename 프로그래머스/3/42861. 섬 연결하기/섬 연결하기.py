efdb68781f7b27b26685755e073a75a58259ca9a
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-06 14:33:01 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,9 +1,8 @@
 def solution(n, costs):
     answer = 0
-    costs.sort(key = lambda x: x[2]) 
+    costs.sort(key = lambda x: x[2])
     link = set([costs[0][0]])
-
-    # Kruskal 알고리즘으로 최소 비용 구하기
+    
     while len(link) != n:
         for v in costs:
             if v[0] in link and v[1] in link:
@@ -12,5 +11,4 @@ def solution(n, costs):
                 link.update([v[0], v[1]])
                 answer += v[2]
                 break
-                
     return answer
