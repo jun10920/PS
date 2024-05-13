5c79d143abcdd22f98371bdf92bc5750e53c9309
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-14 00:04:18 +0900

    [level 3] Title: 정수 삼각형, Time: 75.38 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,16 +1,15 @@
 def solution(triangle):
-    # 각 층까지의 최대 합을 저장하는 dp 배열
-    dp = [row[:] for row in triangle]  # 삼각형의 값을 복사하여 초기화
+    # dp 배열 초기화
+    dp = [[0] * (i+1) for i in range(len(triangle))]
+    dp[0][0] = triangle[0][0]
     
-    # 삼각형의 두 번째 층부터 시작하여 각 요소를 업데이트
     for n in range(1, len(triangle)):
         for i in range(n + 1):
-            # 왼쪽 위에서 내려오는 경우
             if i > 0:
                 dp[n][i] = max(dp[n][i], dp[n-1][i-1] + triangle[n][i])
-            # 바로 위에서 내려오는 경우
             if i < n:
                 dp[n][i] = max(dp[n][i], dp[n-1][i] + triangle[n][i])
-
-    # 마지막 층의 최대값을 반환
+    
     return max(dp[-1])
+
+# 이제 이 함수는 각 층을 거치면서 가능한 최대 합을 계산하고, 마지막 층에서의 최대값을 반환합니다.
