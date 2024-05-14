10649af168ac0c36ebf2e0b46ae92cdb6f52530b
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-15 00:19:28 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,23 +1,25 @@
 from collections import deque
 
 def bfs(begin, target, words):
+    
     queue = deque([(begin, 0)])
     
     while queue:
-        current_word, count = queue.popleft()
+        begin, count = queue.popleft()
         
-        if current_word == target:
+        if begin == target:
             return count
         
         for word in words[:]:
-            if diff_by_one(current_word, word):
-                queue.append((word, count + 1))
-
-def diff_by_one(word1, word2):
-    cnt = sum(1 for a, b in zip(word1, word2) if a != b)
-    return cnt == 1
+            if wordDiffByOne(begin, word):
+                queue.append((word, count +1))
+                words.remove(word)
+                
+def wordDiffByOne(word1, word2):
+    return 1 == sum(1 for a, b in zip(word1, word2) if a !=b)
 
 def solution(begin, target, words):
+    
     if target not in words:
         return 0
     else:
