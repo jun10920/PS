da12643fe906b21f9685c260bdcc7dcccfe054b2
PARKJUNHYOUNG <abide0206@naver.com>
2024-05-14 20:47:52 +0900

    [level 2] Title: 타겟 넘버, Time: 1.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,6 +1,7 @@
 from itertools import product
 
 def solution(numbers, target):
-    l = [(x, -x) for x in numbers]
-    s = list(map(sum, product(*l)))
-    return s.count(target)
+    
+    list1 = [(i, -i) for i in numbers]
+    candidate = list(map(sum, product(*list1)))
+    return candidate.count(target)
