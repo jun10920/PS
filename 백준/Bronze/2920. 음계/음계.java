a363b9215329c12768bae718456bed8fb23e5b54
PARKJUNHYOUNG <abide0206@naver.com>
2024-08-07 11:13:41 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,15 +1,13 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static boolean isUp(int[] arr) {
         for (int i = 0; i < 7; i++) {
-            if (arr[i] >= arr[i + 1]) {
+            if (arr[i] > arr[i + 1]) {
                 return false;
             }
         }
@@ -18,7 +16,7 @@ public class Main {
 
     public static boolean isDown(int[] arr) {
         for (int i = 0; i < 7; i++) {
-            if (arr[i] <= arr[i + 1]) {
+            if (arr[i] < arr[i + 1]) {
                 return false;
             }
         }
@@ -26,16 +24,15 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[8];
 
-        // 입력 값을 배열에 저장
         for (int i = 0; i < 8; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 배열이 오름차순인지, 내림차순인지, 아니면 혼합인지 확인
         if (isUp(arr)) {
             System.out.println("ascending");
         } else if (isDown(arr)) {
@@ -45,3 +42,4 @@ public class Main {
         }
     }
 }
+
