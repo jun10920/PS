f3a3fd29786b49c0306888ef4150b77bb9d90091
PARKJUNHYOUNG <abide0206@naver.com>
2024-08-07 11:41:51 +0900

    [Bronze II] Title: OX퀴즈, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -6,27 +6,27 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < T; i++) {
-            String sen = br.readLine();
+            String sen = br.readLine().trim();
             int score = 0;
-            int current = 0;
+            int current = 1;
 
             for (int j = 0; j < sen.length(); j++) {
                 if (sen.charAt(j) == 'O') {
-                    current++;
-                    score += current;
+                    score += (current++);
                 } else {
-                    current = 0;
+                    current = 1;
                 }
             }
-
-            System.out.println(score);
+            sb.append(score).append("\n");
         }
-
+        System.out.println(sb);
     }
 }
+
