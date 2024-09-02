ef1e6275c872871bdafdc7a87241ebafb740633d
PARKJUNHYOUNG <abide0206@naver.com>
2024-09-02 13:47:53 +0900

    [level 2] Title: 기능개발, Time: 0.53 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,17 +1,24 @@
-from collections import deque
-def solution(prog, speeds):
+def solution(progress, speeds):
+    # answer : 전체 답안을 더하는 배열
+    # temp : 일시적으로 더하는 횟수를 저장하는 배열
+    # progress 배열을 순회하면서, speeds의 배열을 순회하며 같은 인덱스에 더함
+    #   progress의 맨 앞이 100이라면, 없애면서 temp에 +=1
+    #   끝나면 answer append
     
-    dayLeft = deque()
-    for p, s in zip(prog, speeds):
-        dayLeft.append(-((p - 100) // s))
-    print(dayLeft)
+    temp = 0
     answer = []
     
-    while dayLeft:
-        count = 1
-        first = dayLeft.popleft()
-        while dayLeft and first >= dayLeft[0]:
-            dayLeft.popleft()
-            count +=1
-        answer.append(count)
+    while progress:
+        for i in range(len(progress)):
+            progress[i] += speeds[i]
+            
+        while progress and progress[0] >= 100:
+            progress.pop(0)
+            speeds.pop(0)
+            temp += 1
+
+        if temp > 0:
+            answer.append(temp)
+            temp = 0
+    
     return answer
