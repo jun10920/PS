98c8b66b9c357b16a2f97dece7ffd67e7e9450d5
PARKJUNHYOUNG <abide0206@naver.com>
2024-09-04 17:24:28 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.10 ms
+메모리: 10.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 26일 21:02:19
+2024년 09월 04일 17:24:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
-from functools import reduce
 
 def solution(clothes):
-    arr = defaultdict(list)
-    for i in clothes:
-        arr[i[1]] = arr.get(i[1], 0) + 1
-     
-    answer = 1
-    for i in arr.values():
-        answer *= i + 1
     
-    return answer -1
+    temp = defaultdict(int)
+    answer = 1
+    
+    for i in clothes:
+        temp[i[1]] += 1
+    
+    
+    for i in temp.values():
+        answer *= (i+1)        
+    
+    return answer - 1
