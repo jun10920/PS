fecf2791f77abb8ed7a544c0107e9c8e45ef0372
PARKJUNHYOUNG <abide0206@naver.com>
2024-09-04 18:54:48 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 54.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,21 +1,33 @@
 from collections import deque
 
-def solution(length, weight, tw):
+def solution(bridge_length, weight, truck_weights):
+    # 모든 트럭이 다리를 건너는데 걸리는 최소 시간
+    # bridge_length 다리 길이
+    # weight 최대 무게
+    # truck_weights 대기 트럭 리스트
+
+    # 대기 트럭 리스트가 비고, bridge의 총 무게가 0이 될 때까지
+    # 매번 다리 위의 트럭 리스트의 key() 값의 합이 다리 위의 총 무게 보다 적으면
+        # 현재 대기 트럭 리스트에서 하나를 다리 위의 트럭 리스트에 넣음
+        # count += 1
+    # 매번 다리 위의 트럭 리스트의 value() 값 중에 bridge_length보다 커지면
+        # 다리 위의 트럭리스트에서 제거 (0으로 처리)
     
-    count, cw = 0, 0
-    # bridge 생성
-    bridge = deque([0 for _ in range(length)])
-    # tw랑 bridge queue로 생성
-    tw = deque(tw)
-    # cw가 0이 아니고 or tw가 남아있다면
-    while cw != 0 or tw:
+    truckList = deque(truck_weights)
+    bridge = deque([0] * bridge_length)
+    currentW = 0
+    count = 0
+    
+    while truckList or currentW > 0:
         count += 1
-        cw -= bridge.popleft()
+        currentW -= bridge.popleft()
         
-        if tw and cw + tw[0] <= weight:
-            truck = tw.popleft()
-            bridge.append(truck)
-            cw += truck
-        else:
-            bridge.append(0)
+        if truckList:
+            if truckList[0] + currentW <= weight:
+                temp = truckList.popleft()
+                currentW += temp
+                bridge.append(temp)
+            else:
+                bridge.append(0)
+
     return count
