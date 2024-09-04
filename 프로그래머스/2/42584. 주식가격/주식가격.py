5e57efbf110f20832f45f5ec8276b822c371777d
PARKJUNHYOUNG <abide0206@naver.com>
2024-09-04 23:14:55 +0900

    [level 2] Title: 주식가격, Time: 146.67 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,14 @@
 def solution(prices):
     answer = [0] * len(prices)
-    stack = []
     
     for i in range(len(prices)):
-        while stack and prices[i] < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
-        
-    while stack:
-        j = stack.pop()
-        answer[j] = len(prices) - 1 - j
+        count = 0
+        for j in range(i + 1, len(prices)):
+            count += 1
+            if prices[i] > prices[j]:
+                break
+                
+        answer[i] = count
     
     
     return answer
