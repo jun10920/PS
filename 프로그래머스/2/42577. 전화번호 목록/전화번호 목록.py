1f7a9ac206da60c5940d87f969f19ae9b16812a5
BAMLATTE <abide0206@naver.com>
2024-10-06 15:26:21 +0900

    [level 2] Title: 전화번호 목록, Time: 94.03 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,6 @@
-def solution(phone_book):
-    phone_book.sort()
-    
-    for i in range(len(phone_book) - 1):
-        if phone_book[i + 1].startswith(phone_book[i]):
+def solution(pb):
+    pb.sort()
+    for i in range(len(pb) - 1):
+        if pb[i + 1].startswith(pb[i]):
             return False
-    
     return True
