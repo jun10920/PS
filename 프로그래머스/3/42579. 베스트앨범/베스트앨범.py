82740d1c78b90a1e8499877b00fa63d0886b301b
BAMLATTE <abide0206@naver.com>
2024-10-06 21:28:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,28 +1,19 @@
 from collections import defaultdict
 
-def solution(g, p):
-    # 장르가 높은 순
-    # 장르 내에서 많이 재생된 순
-    # 재생 횟수가 같다면 고유 번호가 낮은 순
-    # 각 장르 별로 최대 2개
+def solution(genres, plays):
+    sorted_g = defaultdict(int)
+    sorted_s = defaultdict(list)
     
-    # dict 2개 (장르, 재생 회수 합) / (장르, (재생회수, 고유 번호))
-    # 1번 dict 정렬 - 재생 회수 합이 높은 순으로
-    # 1번 dict의 key 순으로 2번 dict의 장르의 재생회수, 고유 번호 순으로 정렬
-    # 2번 dict의 각 장르의 고유번호를 answer에 기입
+    for g, (i, p) in zip(genres, enumerate(plays)):
+        sorted_g[g] = sorted_g.get(g, 0) + p
+        sorted_s[g].append([i, p])
+        
+    sorted_gl = sorted(sorted_g.keys(), key = sorted_g.get, reverse = True)
+    
     answer = []
-    dict1 = defaultdict(int)
-    dict2 = defaultdict(list)
     
-    for i, (g, p) in enumerate(zip(g, p)):
-        dict1[g] += p
-        dict2[g].append((p, i))
+    for i in sorted_gl:
+        sorted_sl = sorted(sorted_s[i], key = lambda x : (-x[1], x[0]))
+        answer.extend([i[0] for i in sorted_sl][:2])
     
-    sorted_g = sorted(dict1.keys(), key = lambda x : dict1[x], reverse = True)
-        
-    for i in sorted_g:
-        
-        sorted_s = sorted(dict2[i], key = lambda x : (-x[0], x[1]))
-        
-        answer += [song[1] for song in sorted_s[:2]]
     return answer
