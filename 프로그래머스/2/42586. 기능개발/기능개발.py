f5d0158d5b95446c95270b5af6db28c63f0ecc6b
BAMLATTE <abide0206@naver.com>
2024-10-06 22:06:36 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,22 @@
-def solution(progress, speeds):
-    # answer : 전체 답안을 더하는 배열
-    # temp : 일시적으로 더하는 횟수를 저장하는 배열
-    # progress 배열을 순회하면서, speeds의 배열을 순회하며 같은 인덱스에 더함
-    #   progress의 맨 앞이 100이라면, 없애면서 temp에 +=1
-    #   끝나면 answer append
-    
-    temp = 0
-    answer = []
-    
-    while progress:
-        for i in range(len(progress)):
-            progress[i] += speeds[i]
-            
-        while progress and progress[0] >= 100:
-            progress.pop(0)
-            speeds.pop(0)
-            temp += 1
+import math
+from collections import deque
 
-        if temp > 0:
-            answer.append(temp)
-            temp = 0
+def solution(progresses, speeds):
+    answer = []
+    leftDays = []
     
+    for p, s in zip(progresses, speeds):
+        temp = math.ceil((100 - p) / s) 
+        leftDays.append(temp)
+    
+    leftDays = deque(leftDays)
+    
+    while leftDays:
+        temp = leftDays.popleft()
+        cnt = 1
+        while leftDays and leftDays[0] <= temp:
+            leftDays.popleft()
+            cnt += 1
+        answer.append(cnt)
+        
     return answer
