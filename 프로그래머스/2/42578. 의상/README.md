5649f30389f736e80bcd20649d10328b1840c449
BAMLATTE <abide0206@naver.com>
2024-10-06 15:37:11 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 17:24:24
+2024년 10월 06일 15:37:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,17 @@
-from collections import defaultdict
-
+from collections import Counter
 def solution(clothes):
     
-    temp = defaultdict(int)
-    answer = 1
+    temp = []
     
     for i in clothes:
-        temp[i[1]] += 1
+        temp.append(i[1])
+        
+    counter = Counter(temp)
     
+    print(counter.values())
     
-    for i in temp.values():
-        answer *= (i+1)        
+    answer = 1
+    for i in counter.values():
+        answer *= (i + 1)
     
     return answer - 1
