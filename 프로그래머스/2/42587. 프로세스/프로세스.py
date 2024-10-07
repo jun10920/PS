d2c47c12585fb4d398a42e2d5ed1780b8c8a7e49
BAMLATTE <abide0206@naver.com>
2024-10-07 09:46:48 +0900

    [level 2] Title: 프로세스, Time: 2.33 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,34 +1,18 @@
 from collections import deque
 
-def solution(p, location):
+def solution(prior, location):
     
-    # 우선순위와 인덱스를 함께 저장하는 큐
-    # count : 순서
-    # 0번 인덱스 popleft()
-    # 순회하면서 가장 우선순위가 높으면 
-        # count += 1
-        # 인덱스도 location이랑 같으면 return count
-    # 우선순위가 더 큰 게 있으면 append()
-        # count +1
-    
-    tempList = [(v, i) for i, v in enumerate(p)]
-    queue = deque(tempList)
+    pq = deque([])
     count = 0
     
-    while queue:
-        temp = queue.popleft()
-
-        if any(temp[0] < item[0] for item in queue):
-            queue.append(temp)
+    for i,v in enumerate(prior):
+        pq.append([i,v])
+            
+    while pq:
+        temp = pq.popleft()
+        if pq and temp[1] < max(x[1] for x in pq):
+            pq.append(temp)
         else:
-            count += 1
-            if temp[1] == location:
+            count +=1
+            if location == temp[0]:
                 return count
-        
-#         if temp[0] < max(queue, key=lambda x: x[0])[0]:
-#             queue.append(temp)
-#             continue
-        
-#         count += 1
-#         if temp[1] == location:
-#             return count
