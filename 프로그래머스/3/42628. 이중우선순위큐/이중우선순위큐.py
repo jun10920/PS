260beae13f09506fa0faf86e3526c8d6801abd9a
BAMLATTE <abide0206@naver.com>
2024-10-07 20:47:42 +0900

    [level 3] Title: 이중우선순위큐, Time: 804.05 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,24 +1,24 @@
 import heapq
 
 def solution(operations):
-    
-    stack = []
-    
-    for i in operations:
-        command, value = i.split()
+    heap = []  # 최소 힙
+
+    for op in operations:
+        command, value = op.split()
         value = int(value)
-        
-        if command == 'I':
-            heapq.heappush(stack, value)
-        elif stack:
+
+        if command == "I":
+            heapq.heappush(heap, value)
+        elif heap:
             if value == 1:
-                stack.remove(max(stack))
+                heap.remove(max(heap))  # 최댓값 삭제
             else:
-                heapq.heappop(stack)
-                
-        heapq.heapify(stack)
-            
-    if stack:
-        return [max(stack), stack[0]]
+                heapq.heappop(heap)  # 최솟값 삭제
+
+            # 힙 속성 복구
+            heapq.heapify(heap)
+
+    if not heap:
+        return [0, 0]
     else:
-        return [0,0]
+        return [max(heap), heap[0]]  # 최댓값, 최솟값 반환
