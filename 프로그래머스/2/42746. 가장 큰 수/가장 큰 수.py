4718b4ad6afb9b07e76aea41e80e59d73a1c2298
BAMLATTE <abide0206@naver.com>
2024-10-07 17:57:02 +0900

    [level 2] Title: 가장 큰 수, Time: 482.39 ms, Memory: 24 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,18 @@
+from functools import cmp_to_key
+
+def compare(x, y):
+    if x + y > y + x:
+        return - 1
+    elif x + y < y + x:
+        return 1
+    else:
+        return 0
+
 def solution(numbers):
     
-    numbers = list(map(str,numbers))
-    numbers.sort(key = lambda x: x*3, reverse=True)
+    numbers = list(map(str, numbers))
+    
+    numbers.sort(key = cmp_to_key(compare))
+
     answer = ''.join(numbers)
-   
     return answer if answer[0] != '0' else '0'
