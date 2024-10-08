fdf80b4541a5589c2c2546cdc9e0f3e1998454c1
BAMLATTE <abide0206@naver.com>
2024-10-08 11:04:22 +0900

    [level 2] Title: 소수 찾기, Time: 10.05 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.22 ms
+메모리: 12.1 MB, 시간: 10.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 02:40:48
+2024년 10월 08일 11:04:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,26 @@
 from itertools import permutations
 
-def check(n):
-    if n < 2:
+def checkPrime(num):
+    if num < 2:
         return False
     else:
-        for i in range(2, int(n**(1/2)) + 1):
-            if n % i == 0:
+        for i in range(2, int(num**(1/2)) + 1):
+            if num % i == 0:
                 return False
-        return True
+    return True
 
 def solution(numbers):
-    arr = []
+    
+    temp = [i for i in numbers]
+    temp2 = []
     
     for i in range(1, len(numbers) + 1):
-        arr += list(set([i for i in permutations(numbers, i)]))
-        
-    num = [int(''.join(i)) for i in arr]
-    result = set()
-    for i in num:
-        if check(i):
-            result.add(i)
-
-    return len(result)
+        temp2 += list(permutations(temp, i))
+    
+    temp3 = set([int(''.join(i)) for i in temp2])
+            
+    answer = 0
+    for i in temp3:
+        if checkPrime(i):
+            answer += 1
+    return answer
