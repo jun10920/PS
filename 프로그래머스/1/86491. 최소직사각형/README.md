3b8ab3461fdd67019316bba72423c0533f912384
BAMLATTE <abide0206@naver.com>
2024-10-08 10:05:15 +0900

    [level 1] Title: 최소직사각형, Time: 2.65 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 1.76 ms
+메모리: 11.6 MB, 시간: 2.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 01일 02:11:44
+2024년 10월 08일 10:05:12
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,14 +1,11 @@
 def solution(sizes):
     
-    max1 = 0
-    max2 = 0
-    
+    temp1 = []
+    temp2 = []
     for i in sizes:
         i.sort(reverse = True)
         
-        if i[0] > max1:
-            max1 = i[0]
-        if i[1] > max2:
-            max2 = i[1]
-    
-    return max1 * max2
+        temp1.append(i[0])
+        temp2.append(i[1])
+
+    return max(temp1) * max(temp2)
