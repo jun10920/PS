3733439ccb1c1ed0d8b1e7f9f148fe83ad8df0ab
BAMLATTE <abide0206@naver.com>
2024-10-08 10:28:07 +0900

    [level 1] Title: 모의고사, Time: 4.81 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,28 @@
 def solution(answers):
-    man1 = [1, 2, 3, 4, 5]
-    man2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    num1, num2, num3 = 0, 0, 0
     
-    score = [0, 0, 0]
+    answer1 = [1,2,3,4,5]
+    answer2 = [2,1,2,3,2,4,2,5]
+    answer3 = [3,3,1,1,2,2,4,4,5,5]
     
-    for i in range(len(answers)):
-        if answers[i] == man1[i%5]:
-            score[0] += 1
-        if answers[i] == man2[i%8]:
-            score[1] += 1
-        if answers[i] == man3[i%10]:
-            score[2] += 1
+    for i, v in enumerate(answers):
+        if answer1[i % 5] == answers[i]:
+            num1 += 1
+        if answer2[i % 8] == answers[i]:
+            num2 += 1
+        if answer3[i % 10] == answers[i]:
+            num3 += 1
     
-    maxAn = max(score)
+    temp = []
+    temp+=[num1, num2, num3]
     
-    return [i + 1 for i, v in enumerate(score) if v == maxAn]
+    print(temp)
+    
+    answer = []
+    
+    top = max(temp)
+    for i, v in enumerate(temp):
+        if v == top:
+            answer.append(i+1)
+    
+    return answer
