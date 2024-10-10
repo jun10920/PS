5f6ab3565a1d7d9bbbc8233ec73226ae4dd6c85e
BAMLATTE <abide0206@naver.com>
2024-10-11 00:33:49 +0900

    [level 2] Title: 모음 사전, Time: 1.69 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 1.61 ms
+메모리: 10.1 MB, 시간: 1.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 04일 00:37:48
+2024년 10월 11일 00:33:44
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -2,14 +2,14 @@ from itertools import product
 
 def solution(word):
     
-    arr1 = ['A', 'E', 'I', 'O', 'U']
-    arr2 = []
+    temp = ['A', 'E', 'I', 'O', 'U']
+    senList = []
     
-    for i in range(1, len(arr1) + 1):
-        for combination in product(arr1, repeat = i):
-            arr2.append(''.join(combination))
-            
-    arr2.sort()         
-    answer = arr2.index(word) + 1
+    for i in range(1, 6):
+        for j in product(temp, repeat = i):
+            senList.append(''.join(j))
     
-    return answer
+        
+    senList.sort()
+
+    return senList.index(word) + 1
