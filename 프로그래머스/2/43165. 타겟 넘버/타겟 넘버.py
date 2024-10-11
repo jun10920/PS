e51387b3cb758a9f87987c4a6dd5555c3c8e4d66
BAMLATTE <abide0206@naver.com>
2024-10-11 17:40:38 +0900

    [level 2] Title: 타겟 넘버, Time: 350.62 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,7 +1,8 @@
-from itertools import product
+def dfs(numbers, target, index, current):
+    if index == len(numbers):
+        return 1 if current == target else 0
+    else:
+        return dfs(numbers, target, index + 1, current + numbers[index]) +dfs(numbers, target, index + 1, current - numbers[index])
 
 def solution(numbers, target):
-    
-    list1 = [(i, -i) for i in numbers]
-    candidate = list(map(sum, product(*list1)))
-    return candidate.count(target)
+    return dfs(numbers, target, 0, 0)
