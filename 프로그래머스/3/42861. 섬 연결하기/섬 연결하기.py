93d46bf327f41c1b641955f5c1a58c21a719c535
BAMLATTE <abide0206@naver.com>
2024-10-19 00:22:08 +0900

    [level 3] Title: 섬 연결하기, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,29 +1,40 @@
-import heapq
+class UnionFind:
+    def __init__(self, size):
+        self.root = list(range(size))
+        self.rank = [1] * size
+    
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
 
 def solution(n, costs):
-
-    graph = [[] for _ in range(n)]
-    for u, v, cost in costs:
-        graph[u].append((cost,v))
-        graph[v].append((cost,u))
-        
-    visited = [False] * n # 노드 방문 여부
-    min_heap = [(0, 0)] # (간선 가중치, 시작 노드), 0번 노드에서 시작
-    total_cost = 0
+    
+    costs.sort(key = lambda x : x[2])
+    uf = UnionFind(n)
+    answer = 0
     edges_used = 0
     
-    while edges_used < n:
-        cost, u = heapq.heappop(min_heap)
-        
-        if visited[u]:
-            continue
-        
-        visited[u] = True
-        total_cost += cost
-        edges_used += 1
-        
-        for edge_cost, v in graph[u]:
-            if not visited[v]:
-                heapq.heappush(min_heap, (edge_cost, v))
-    
-    return total_cost
+    for u, v, cost in costs:
+        if not uf.connected(u, v):
+            uf.union(u, v)
+            answer += cost
+            edges_used += 1
+            if edges_used == n - 1:
+                break
+    return answer
