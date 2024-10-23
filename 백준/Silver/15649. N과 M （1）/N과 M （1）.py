c3b6ec0dca7939d8aa5a4290c18749d4e38e659a
BAMLATTE <abide0206@naver.com>
2024-10-23 10:46:25 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,12 +1,19 @@
 n, m = list(map(int, input().split()))
 
-import itertools
+def back_tracking(arr, now_length, used_num):
+  if now_length == m:
+    print(*arr)
+    return
 
-nums = [i for i in range(1, n + 1)]
+  for i in range(1, n + 1):
+    if not used_num[i]:
+      arr.append(i)
+      used_num[i] = True
+      back_tracking(arr, now_length + 1, used_num)
+      arr.pop()
+      used_num[i] = False
+      
 
-arr = itertools.permutations(nums, m)
-
-for i in arr:
-  for j in i:
-    print(j, end=' ')
-  print()
+nums = [i + 1 for i in range(n+1)]
+used_num = [False] * (n + 1)
+back_tracking([], 0, used_num)
