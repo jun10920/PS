ade10742e229769409b16b3805e08d6c482d6ff1
BAMLATTE <abide0206@naver.com>
2024-10-23 23:16:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 84 ms, Memory: 34628 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,26 +1,24 @@
 from collections import deque
+
 n, k = map(int, input().split())
 
-answer = float('inf')
+def bfs(start, end):
+    if start == end:
+        return 0
+    
+    visited = [False] * 100001  # 배열 크기를 100001로 설정
+    visited[start] = True
+    count = 0
+    queue = deque([(start, count)])
 
-def bfs(start, target):
-  if start == target:
-      return 0
+    while queue:
+        current, count = queue.popleft()
 
-  queue = deque([(start, 0)])  # (현재 위치, 이동 횟수)
-  visited = [False] * 100001  # 방문 체크 배열 (문제 조건에 따라 범위 설정)
-  visited[start] = True
+        for next in (current + 1, current - 1, current * 2):
+            if 0 <= next <= 100000 and not visited[next]:  # 범위를 0 <= next <= 100000으로 수정
+                if next == end:
+                    return count + 1
+                queue.append((next, count + 1))
+                visited[next] = True  # 방문 체크를 잊지 않도록 추가
 
-  while queue:
-      current, count = queue.popleft()
-
-      # 가능한 이동 (현재 위치 +1, -1, *2)
-      for next_position in (current + 1, current - 1, current * 2):
-          if 0 <= next_position <= 100000 and not visited[next_position]:
-              if next_position == target:
-                  return count + 1
-              queue.append((next_position, count + 1))
-              visited[next_position] = True
-
-# BFS 실행
 print(bfs(n, k))
