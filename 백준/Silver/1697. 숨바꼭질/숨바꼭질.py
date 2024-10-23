3474627a1959f0db656b133cdac348c61d9c9ece
BAMLATTE <abide0206@naver.com>
2024-10-23 23:17:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 84 ms, Memory: 34292 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,22 +3,23 @@ from collections import deque
 n, k = map(int, input().split())
 
 def bfs(start, end):
-    if start == end:
-        return 0
-    
-    visited = [False] * 100001  # 배열 크기를 100001로 설정
-    visited[start] = True
-    count = 0
-    queue = deque([(start, count)])
+  if start == end:
+    return 0
+  
+  visited = [False] * 100001
+  visited[start] = True
+  count = 0
+  queue = deque([(start, count)])
 
-    while queue:
-        current, count = queue.popleft()
+  while queue:
+    current, count = queue.popleft()
 
-        for next in (current + 1, current - 1, current * 2):
-            if 0 <= next <= 100000 and not visited[next]:  # 범위를 0 <= next <= 100000으로 수정
-                if next == end:
-                    return count + 1
-                queue.append((next, count + 1))
-                visited[next] = True  # 방문 체크를 잊지 않도록 추가
+    for next in (current + 1, current -1, current * 2):
+      if 0 <= next <= 100000 and not visited[next]:
+        if next == end:
+          return count + 1
+        queue.append((next, count + 1))
+        visited[next] = True
 
 print(bfs(n, k))
+      
