0bf7e3e603aec9570c9e01282382347e30cdcbc9
BAMLATTE <abide0206@naver.com>
2024-10-25 22:34:02 +0900

    [Gold V] Title: 보물섬, Time: 684 ms, Memory: 119340 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,37 +1,29 @@
 from collections import deque
 
-y, x = map(int, input().split())
-
-maps = [list(input().strip()) for _ in range(y)]
+h, w = map(int, input().split())
+maps = [input() for _ in range(h)]
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
+
 def bfs(start_y, start_x):
-    visited = [[-1] * x for _ in range(y)]
-    queue = deque([(start_y, start_x)])
-    visited[start_y][start_x] = 0  # 시작 위치의 거리를 0으로 설정
+    queue = deque([(start_y, start_x, 0)])
+    visited = [[False] * w for _ in range(h)]
+    visited[start_y][start_x] = True
+    max_dis = 0
 
-    max_distance = 0
-    
     while queue:
-        current_y, current_x = queue.popleft()
-
+        y, x, dis = queue.popleft()
+        max_dis = max(max_dis, dis)
         for dy, dx in directions:
-            ny, nx = current_y + dy, current_x + dx
-            
-            # 범위 내에 있고, 육지이며, 아직 방문하지 않은 경우
-            if 0 <= ny < y and 0 <= nx < x and maps[ny][nx] == 'L' and visited[ny][nx] == -1:
-                visited[ny][nx] = visited[current_y][current_x] + 1
-                max_distance = max(max_distance, visited[ny][nx])
-                queue.append((ny, nx))
-                
-    return max_distance
+            ny, nx = y + dy, x + dx
+            if 0 <= ny < h and 0 <= nx < w and maps[ny][nx] != 'W' and not visited[ny][nx]:
+                visited[ny][nx] = True
+                queue.append((ny, nx, dis + 1))
+    return max_dis
 
-max_distance = 0
-
-# 모든 육지에서 BFS를 수행하여 최대 거리 찾기
-for i in range(y):
-    for j in range(x):
-        if maps[i][j] == 'L':
-            max_distance = max(max_distance, bfs(i, j))
-
-print(max_distance)
+answer = float('-inf')
+for i in range(h):
+    for j in range(w):
+        if maps[i][j] != 'W':
+            answer = max(answer, bfs(i, j))
+print(answer)
